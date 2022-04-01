6e82b6f88442e54520843cdc5f7371fe411e7b4f
Nadheva Derdika <59642869+nadheva@users.noreply.github.com>
2022-04-01 18:41:06 +0700

    payload

--- app/Http/Controllers/SewaPerangkatController.php
+++ app/Http/Controllers/SewaPerangkatController.php
@@ -5,12 +5,27 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\SewaPerangkat;
 use App\Models\Perangkat;
+use Midtrans\Snap;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Auth\Events\Validated;
 
 class SewaPerangkatController extends Controller
 {
+
+    public function __construct(Request $request)
+    {
+        $this->middleware('auth')->except('notificationHandler');
+
+        $this->request = $request;
+        // Set midtrans configuration
+        \Midtrans\Config::$serverKey    = config('services.midtrans.serverKey');
+        \Midtrans\Config::$isProduction = config('services.midtrans.isProduction');
+        \Midtrans\Config::$isSanitized  = config('services.midtrans.isSanitized');
+        \Midtrans\Config::$is3ds        = config('services.midtrans.is3ds');
+    }
+
     public function index()
     {
         $perangkat = Perangkat::all();
@@ -23,8 +38,9 @@ class SewaPerangkatController extends Controller
         return view('user.sewa-perangkat.sewa', compact('perangkat'));
     }
 
-    public function store(Request $request, $id)
+    public function store()
     {
+        DB::transaction(function(Request $request, $id) {
         $length = 10;
         $random = '';
         for ($i = 0; $i < $length; $i++) {
@@ -55,7 +71,7 @@ class SewaPerangkatController extends Controller
             'keperluan' => $request->keperluan,
             'proses' => $request->proses,
             'status' => 'pending',
-            'snap_token' => $request->snap_token,
+            // 'snap_token' => $request->snap_token,
             'grand_total' => $request->grand_total
         ]);
 
@@ -63,6 +79,129 @@ class SewaPerangkatController extends Controller
                                         ->update([
                                             'stok' => ($sewa_perangkat->perangkat->stok - 1),
                                         ]);
+        $sewa_perangkat->order()->create([
+            'sewa_perangkat_id' => $sewa_perangkat->id
+        ]);
+
+        $payload = [
+            'transaction_details' => [
+                'order_id' => $sewa_perangkat->invoice,
+                'gross_amount' => $sewa_perangkat->grand_total,
+            ],
+            'customer_details' => [
+                'first_name' => $user->name,
+                'email' => $user->email,
+            ]
+        ];
+
+        //snap token
+        $snapToken = Snap::getSnapToken($payload);
+        $sewa_perangkat->snap_token = $snapToken;
+        $sewa_perangkat->save();
+
+        });
+    }
+
+    public function notificationHandler(Request $request)
+    {
+        $payload      = $request->getContent();
+        $notification = json_decode($payload);
+
+        $validSignatureKey = hash("sha512", $notification->order_id . $notification->status_code . $notification->gross_amount . config('services.midtrans.serverKey'));
+
+        if ($notification->signature_key != $validSignatureKey) {
+            return response(['message' => 'Invalid signature'], 403);
+        }
+
+        $transaction  = $notification->transaction_status;
+        $type         = $notification->payment_type;
+        $orderId      = $notification->order_id;
+        $fraud        = $notification->fraud_status;
+
+        //data tranaction
+        $data_transaction = SewaPerangkat::where('invoice', $orderId)->first();
+
+        if ($transaction == 'capture') {
+
+            // For credit card transaction, we need to check whether transaction is challenge by FDS or not
+            if ($type == 'credit_card') {
+
+              if($fraud == 'challenge') {
+
+                /**
+                *   update invoice to pending
+                */
+                $data_transaction->update([
+                    'status' => 'pending'
+                ]);
+
+              } else {
+
+                /**
+                *   update invoice to success
+                */
+                $data_transaction->update([
+                    'status' => 'success'
+                ]);
+
+              }
+
+            }
+
+        } elseif ($transaction == 'settlement') {
+
+            /**
+            *   update invoice to success
+            */
+            $data_transaction->update([
+                'status' => 'success'
+            ]);
+
+
+        } elseif($transaction == 'pending'){
+
+
+            /**
+            *   update invoice to pending
+            */
+            $data_transaction->update([
+                'status' => 'pending'
+            ]);
+
+
+        } elseif ($transaction == 'deny') {
+
+
+            /**
+            *   update invoice to failed
+            */
+            $data_transaction->update([
+                'status' => 'failed'
+            ]);
+
+
+        } elseif ($transaction == 'expire') {
+
+
+            /**
+            *   update invoice to expired
+            */
+            $data_transaction->update([
+                'status' => 'expired'
+            ]);
+
+
+        } elseif ($transaction == 'cancel') {
+
+            /**
+            *   update invoice to failed
+            */
+            $data_transaction->update([
+                'status' => 'failed'
+            ]);
+
+        }
+
     }
 
     public function edit($id)
