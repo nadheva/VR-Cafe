319387e9b3d716782c679d5c741280ee12e212f8
Nadheva Derdika <59642869+nadheva@users.noreply.github.com>
2022-05-21 22:25:17 +0700

    push

--- resources/views/user/dashboard/index.blade.php
+++ resources/views/user/dashboard/index.blade.php
@@ -181,13 +181,13 @@
                             <i class="fa fa-ellipsis-h text-secondary" aria-hidden="true"></i>
                           </a>
                           <ul class="dropdown-menu px-2 py-3 ms-sm-n4 ms-n5" aria-labelledby="dropdownTable2" style="">
-                            {{-- @if(is_null($i->sewa_perangkat_id) && is_null($i->denda_id))
-                            <li><a class="dropdown-item border-radius-md" href="{{route('user-transaksi-studio.show', $i->sewa_ruang->id)}}">Lihat Selengkapnya</a></li>
+                            @if(is_null($i->sewa_perangkat_id) && is_null($i->denda_id))
+                            <li><a class="dropdown-item border-radius-md" href="{{route('user-transaksi-studio.show', $id = $i->sewa_ruang_id)}}">Lihat Selengkapnya</a></li>
                             @elseif(is_null($i->sewa_perangkat_id) && is_null($i->sewa_ruang_id))
-                            <li><a class="dropdown-item border-radius-md" href="{{route('user-denda.show', $i->denda->id)}}">Lihat Selengkapnya</a></li>
+                            <li><a class="dropdown-item border-radius-md" href="{{route('user-denda.show', $id = $i->denda_id)}}">Lihat Selengkapnya</a></li>
                             @elseif(is_null($i->sewa_denda_id) && is_null($i->sewa_ruang_id))
-                            <li><a class="dropdown-item border-radius-md" href="{{route('user-transaksi-perangkat.show', $i->sewa_perangkat->id)}}">Lihat Selengkapnya</a></li> --}}
-                        {{-- @endif --}}
+                            <li><a class="dropdown-item border-radius-md" href="{{route('user-transaksi-perangkat.show', $id = $i->sewa_perangkat_id)}}">Lihat Selengkapnya</a></li>
+                            @endif
                             </ul>
                         </div>
                       </div>
