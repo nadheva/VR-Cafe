c6fb1bdb49aa1891b2f768ba8c0df5f958e9bc10
Nadheva Derdika <59642869+nadheva@users.noreply.github.com>
2022-07-18 18:44:22 +0700

    push

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,26 +10,54 @@ class UserController extends Controller
 {
     public function index()
     {
+        $superadmin = User::where('id', '=', '1');
+        $user = User::latest()->get()->except($superadmin);
+        return view('admin.user.index', compact('user'));
 
     }
 
     public function store(Request $request)
     {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+            'role' => 'required'
+        ]);
 
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+            'role' => $request->role
+        ]);
+        Alert::success('Success', 'User berhasil ditambahkan!');
+        return redirect()->back();
     }
 
     public function show($id)
     {
-
+        $id1 = decrypt($id);
+        $user = User::where('id', $id1)->first();
+        return view('admin.user.index', compact('user'));
     }
 
     public function update(Request $request, $id)
     {
-
+        $user = User::findOrfail($id);
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->role = $request->role;
+        $user->save();
+        Alert::info('Info', 'User berhasil diedit!');
+        return redirect()->back();
     }
 
     public function destroy($id)
     {
-        
+        User::where('id', $id)->delete();
+        Alert::warning('Warning', 'User berhasil dihapus!');
+        return redirect()->back();
     }
 }
